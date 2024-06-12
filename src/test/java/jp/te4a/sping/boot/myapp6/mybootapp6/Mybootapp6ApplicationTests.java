3fdadeacd2845438d8f71ee8176e110b4f5f26d2
te210879 <tejc210879@gmail.com>
2024-06-12 12:48:53 +0900

    課題６登録

---
+++ src/test/java/jp/te4a/sping/boot/myapp6/mybootapp6/Mybootapp6ApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.te4a.sping.boot.myapp6.mybootapp6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mybootapp6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
