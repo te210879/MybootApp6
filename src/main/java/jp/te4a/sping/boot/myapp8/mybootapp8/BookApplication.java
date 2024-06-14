810179a1ef75da42bfea8ec0a3ccb7a23807e3f2
te210879 <tejc210879@gmail.com>
2024-06-14 13:07:21 +0900

    kadai8登録

---
+++ src/main/java/jp/te4a/sping/boot/myapp8/mybootapp8/BookApplication.java
@@ -0,0 +1,11 @@
+package jp.te4a.sping.boot.myapp8.mybootapp8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookApplication {
+ public static void main(String[] args) {
+ SpringApplication.run(BookApplication.class, args);
+ }
+}
